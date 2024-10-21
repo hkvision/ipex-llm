aa3e0f4c0e2fb923335ad8d9df127dd28aa782f4
Kai Huang <huangkaivision@gmail.com>
2024-10-21 19:09:34 +0800

    modify qwen

--- python/llm/src/ipex_llm/transformers/npu_models/qwen2_mp.py
+++ python/llm/src/ipex_llm/transformers/npu_models/qwen2_mp.py
@@ -138,26 +138,6 @@ class LowBitQwenMultiDecoderlayer(LLMBaseNNFactory):
             attention_mask = self.create_input_op((self.batch_size, 1, self.seq_len, self.seq_len))
 
         position_ids = self.create_input_op((self.batch_size, self.seq_len))
-        past_keys = []
-        past_values = []
-        if mode == "decode":
-            for i in range(num_layers):
-                past_key = self.create_cache_op(
-                    (self.batch_size, self.num_key_value_heads, self.max_seq_len, self.head_dim)
-                )
-                if transpose_value:
-                    past_value = self.create_cache_op(
-                        (self.batch_size, self.num_key_value_heads, self.head_dim, self.max_seq_len)
-                    )
-                else:
-                    past_value = self.create_cache_op(
-                        (self.batch_size, self.num_key_value_heads, self.max_seq_len, self.head_dim)
-                    )
-                past_keys.append(past_key)
-                past_values.append(past_value)
-        else:
-            past_keys = [None] * num_layers
-            past_values = [None] * num_layers
 
         if input_layernorm_weights is None:
             input_layernorm_weights = []
@@ -195,6 +175,27 @@ class LowBitQwenMultiDecoderlayer(LLMBaseNNFactory):
             q_biases = [self.constant(w) for w in q_biases]
             k_biases = [self.constant(w) for w in k_biases]
             v_biases = [self.constant(w) for w in v_biases]
+
+        past_keys = []
+        past_values = []
+        if mode == "decode":
+            for i in range(num_layers):
+                past_key = self.create_cache_op(
+                    (self.batch_size, self.num_key_value_heads, self.max_seq_len, self.head_dim)
+                )
+                if transpose_value:
+                    past_value = self.create_cache_op(
+                        (self.batch_size, self.num_key_value_heads, self.head_dim, self.max_seq_len)
+                    )
+                else:
+                    past_value = self.create_cache_op(
+                        (self.batch_size, self.num_key_value_heads, self.max_seq_len, self.head_dim)
+                    )
+                past_keys.append(past_key)
+                past_values.append(past_value)
+        else:
+            past_keys = [None] * num_layers
+            past_values = [None] * num_layers
 
         hidden_states = input
 
